3a330b6bd5ec4592296de2c9761b391a7553bfc9
floyd <none>
2018-12-17 16:07:23 +0500

    fix

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import hashlib
 import os
-from collections import Counter
 
 def get_files(dir):
+  list = []
   files = [os.path.join(r,file) for r,d,f in os.walk(dir) for file in f]
+  for file in files:
+      md5 = hashlib.md5(open(file,'rb').read()).hexdigest()
+      if md5 not in list:
+          list.append(md5)
+      else: print('Найден дубль: ', file)
   return files
 
-def get_md5(dir):
-    files = get_files(dir)
-    md5_list = []
-    for file in files:
-      md5 = hashlib.md5(open(file,'rb').read()).hexdigest()
-      md5_list.append(md5)
-    return md5_list
+dir = 'C:\\Users\\floyd\\Desktop\\Tor Browser'
 
-def find_duplicates(dir):
-    md5_sums = get_md5(dir)
-    counter = Counter(md5_sums)
-    return print('Найдены следующие дубли по md5: ', counter)
-
-print('введите путь для поиска дубликатов файлов по md5: ')
-dir = input()
-find_duplicates(dir)
+get_files(dir)
