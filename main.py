1b0538674e10f8fdf6db167c4510a8c62659a534
floyd <none>
2018-12-17 16:31:54 +0500

    fix-3

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import hashlib
 import os
 
-def get_files(dir):
+def get_dublicates(dir):
   list = []
+  list_elements = []
   files = [os.path.join(r,file) for r,d,f in os.walk(dir) for file in f]
   for file in files:
       md5 = hashlib.md5(open(file,'rb').read()).hexdigest()
       if md5 not in list:
           list.append(md5)
-      else: print('Найден дубль: ', file)
-  return files
+      else: list_elements.append(file)
+  return list_elements
 
-dir = 'C:\\Users\\floyd\\Desktop\\Tor Browser'
-
-get_files(dir)
+dir = 'F:\\projects'
+x = get_dublicates(dir)
+print('Список дублей:',x)
