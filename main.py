01e00f441e3bc64e05bab6f4a813b9d61d801ede
floyd <none>
2018-12-17 13:33:48 +0500

    fixes

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import glob
 from collections import Counter
 
 
-
 def get_files(dir):
   files = [os.path.join(r,file) for r,d,f in os.walk(dir) for file in f]
   return files
@@ -18,20 +17,11 @@ def get_md5(dir):
       md5_list.append(md5)
     return md5_list
 
-
-
 def find_duplicates(dir):
     md5_sums = get_md5(dir)
     counter = Counter(md5_sums)
     return print('Найдены следующие дубли по md5: ', counter)
 
-
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(help='Введите путь',required=True)
-    args = parser.parse_args()
-    return args
-
-args = create_parser()
-dir = (args.p).replace('/','\\\\')
-print(dir)
+print('введите путь для поиска дубликатов файлов по md5: ')
+dir = input()
+find_duplicates(dir)
